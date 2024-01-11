33bd8c3768686d9baa1fe3b38ff3ccce83e57af0
Nancy-Sabrina-Ishimwe <ishimwenancysabrina1@gmail.com>
2024-01-11 16:19:59 +0200

    To remove the posted house

--- routes/house.routes.js
+++ routes/house.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const houseRouter = express.Router();
-const {create,list,update } = require ('../controller/house.controller');
+const {create,list,update, remove} = require ('../controller/house.controller');
 
 
 houseRouter.post('/add', create);
 houseRouter.get('/list', list);
 houseRouter.put('/update', update);
+houseRouter.delete('/delete', remove);
 
 module.exports = houseRouter;
